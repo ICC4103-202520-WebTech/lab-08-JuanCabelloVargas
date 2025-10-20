6118bd8f9e9de591ec64ef3e96c7eb21202bf37a
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-20 16:45:12 -0300

    feat: add cancancan permissions to controllers

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,14 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
-  before_action :set_recipe_public, only: [ :show ]
-  before_action :set_owned_recipe, only: [ :edit, :update, :destroy ]
+
+  load_and_authorize_resource except: :index
 
   def index
     if user_signed_in? && params[:mine].present?
       @recipes = current_user.recipes.order(created_at: :desc)
       flash.now[:alert] = "You don't have recipes." if @recipes.empty?
     else
-      @recipes = Recipe.all
+      @recipes = Recipe.accessible_by(current_ability).order(created_at: :desc)
     end
   end
 
@@ -16,15 +16,15 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = current_user.recipes.build
   end
 
   def create
-    recipe = current_user.recipes.build recipe_params
-    if recipe.save
-      redirect_to recipe_path(recipe)
+    @recipe.user ||= current_user
+
+    if @recipe.save
+      redirect_to @recipe, notice: "Recipe created successfully."
     else
-      redirect_to new_recipe_path
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -32,32 +32,21 @@ class RecipesController < ApplicationController
   end
 
   def update
-    if @recipe.update recipe_params
-      redirect_to recipe_path(@recipe)
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: "Recipe updated."
     else
-      redirect_to edit_recipe_path(@recipe)
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @recipe.destroy
-    redirect_to recipe_path
+    redirect_to recipes_path, notice: "Recipe deleted."
   end
 
   private
-    def recipe_params
-      params.require(:recipe).permit(:title, :content, :difficulty, :cook_time)
-    end
 
-    def set_recipe_public
-      @recipe = Recipe.find(params[:id])
-    end
-
-    def set_owned_recipe
-      @recipe = current_user.recipes.find(params[:id])
-
-      unless @recipe
-        redirect_to recipes_path, alert: "You do not have permission to edit or destroy"
-      end
-    end
+  def recipe_params
+    params.require(:recipe).permit(:title, :content, :difficulty, :cook_time)
+  end
 end
